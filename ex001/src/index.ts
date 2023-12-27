8aa3c0f2c6a5dfc34750ce0a19c7ecd2b037e108
Joao <dev.joaogustavo.dev@gmail.com>
2023-12-27 02:47:04 -0300

    studying typescript | understanding tuples

--- ex001/src/index.ts
+++ ex001/src/index.ts
@@ -11,3 +11,9 @@ function sum(num1: number, num2: number) {
 }
 
 sum(1, 2)
+
+//Arrays
+let myArr: number[] = [1, 2, 2,2,233,23,23,2,2 ]
+
+//Tuples
+let myArr2: [number, string, boolean] = [2, "name", false]
