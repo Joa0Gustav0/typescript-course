dd49df798f2d3d40cc84124fc8b9b8efc6f39bdb
Joao <dev.joaogustavo.dev@gmail.com>
2023-12-27 01:14:22 -0300

    studying typescript | declaring types and working with functions

---
+++ ex001/dist/index.js
@@ -0,0 +1,7 @@
+"use strict";
+let greetings = "Hello, World!";
+let greetings2 = "Hello, World!";
+function sum(num1, num2) {
+    console.log(num1 + num2);
+}
+sum(1, 2);
---
+++ ex001/src/index.ts
@@ -0,0 +1,13 @@
+//Declaring a variable
+//annotation - most prefered to be used into functions
+let greetings: string = "Hello, World!"
+
+//Inference
+let greetings2 = "Hello, World!"
+
+//Working with functions
+function sum(num1: number, num2: number) {
+    console.log(num1 + num2)
+}
+
+sum(1, 2)
