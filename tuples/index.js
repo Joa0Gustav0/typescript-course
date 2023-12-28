6b410e2d7a0668f4876cca8bccebb7f83b9f9674
Joao <dev.joaogustavo.dev@gmail.com>
2023-12-28 01:14:02 -0300

    studying typescript | rename directories and study tuples

---
+++ tuples/index.js
@@ -0,0 +1,7 @@
+//Tuples
+var myTuple = ["Gustavo", 2409, true];
+//Arrays + Tuples
+var myTuplesArray = [
+    ["Gustavo", 2409, true],
+    ["sadhskj", 32789372, false]
+];
---
+++ tuples/index.ts
@@ -0,0 +1,10 @@
+//Tuples
+
+const myTuple: [string, number, boolean] = ["Gustavo", 2409, true];
+
+//Arrays + Tuples
+
+const myTuplesArray: [string, number, boolean][] = [
+    ["Gustavo", 2409, true],
+    ["sadhskj", 32789372, false]
+]
