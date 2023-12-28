f801c38772e98f27c1df0f09a6788d7369286d0b
Joao <dev.joaogustavo.dev@gmail.com>
2023-12-27 21:23:23 -0300

    studying typescript | arrays

---
+++ ex003/index.js
@@ -0,0 +1,14 @@
+//Working with arrays
+//Defining the type for an array
+var myArray = [];
+var myArray2 = [];
+/* var masterArray: Array<Array<number>> = [
+    [2,3,4],
+    [6757,765765,1312],
+    [32313,32313,3],
+]
+var masterArray: number[][] = [
+    [2,3,4],
+    [6757,765765,1312],
+    [32313,32313,3],
+] */ 
