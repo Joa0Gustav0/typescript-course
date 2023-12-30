4d217ea10a6228fc372ee47faca48c5e0d2bac6a
Joao <dev.joaogustavo.dev@gmail.com>
2023-12-30 09:48:43 -0300

    studying typescript | property decorator

--- decorators/index.js
+++ decorators/index.js
@@ -1,95 +1,53 @@
-var __extends = (this && this.__extends) || (function () {
-    var extendStatics = function (d, b) {
-        extendStatics = Object.setPrototypeOf ||
-            ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
-            function (d, b) { for (var p in b) if (Object.prototype.hasOwnProperty.call(b, p)) d[p] = b[p]; };
-        return extendStatics(d, b);
-    };
-    return function (d, b) {
-        if (typeof b !== "function" && b !== null)
-            throw new TypeError("Class extends value " + String(b) + " is not a constructor or null");
-        extendStatics(d, b);
-        function __() { this.constructor = d; }
-        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
-    };
-})();
-var __esDecorate = (this && this.__esDecorate) || function (ctor, descriptorIn, decorators, contextIn, initializers, extraInitializers) {
-    function accept(f) { if (f !== void 0 && typeof f !== "function") throw new TypeError("Function expected"); return f; }
-    var kind = contextIn.kind, key = kind === "getter" ? "get" : kind === "setter" ? "set" : "value";
-    var target = !descriptorIn && ctor ? contextIn["static"] ? ctor : ctor.prototype : null;
-    var descriptor = descriptorIn || (target ? Object.getOwnPropertyDescriptor(target, contextIn.name) : {});
-    var _, done = false;
-    for (var i = decorators.length - 1; i >= 0; i--) {
-        var context = {};
-        for (var p in contextIn) context[p] = p === "access" ? {} : contextIn[p];
-        for (var p in contextIn.access) context.access[p] = contextIn.access[p];
-        context.addInitializer = function (f) { if (done) throw new TypeError("Cannot add initializers after decoration has completed"); extraInitializers.push(accept(f || null)); };
-        var result = (0, decorators[i])(kind === "accessor" ? { get: descriptor.get, set: descriptor.set } : descriptor[key], context);
-        if (kind === "accessor") {
-            if (result === void 0) continue;
-            if (result === null || typeof result !== "object") throw new TypeError("Object expected");
-            if (_ = accept(result.get)) descriptor.get = _;
-            if (_ = accept(result.set)) descriptor.set = _;
-            if (_ = accept(result.init)) initializers.unshift(_);
-        }
-        else if (_ = accept(result)) {
-            if (kind === "field") initializers.unshift(_);
-            else descriptor[key] = _;
-        }
-    }
-    if (target) Object.defineProperty(target, contextIn.name, descriptor);
-    done = true;
-};
-var __runInitializers = (this && this.__runInitializers) || function (thisArg, initializers, value) {
-    var useValue = arguments.length > 2;
-    for (var i = 0; i < initializers.length; i++) {
-        value = useValue ? initializers[i].call(thisArg, value) : initializers[i].call(thisArg);
-    }
-    return useValue ? value : void 0;
-};
-var __setFunctionName = (this && this.__setFunctionName) || function (f, name, prefix) {
-    if (typeof name === "symbol") name = name.description ? "[".concat(name.description, "]") : "";
-    return Object.defineProperty(f, "name", { configurable: true, value: prefix ? "".concat(prefix, " ", name) : name });
+"use strict";
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
 };
 function logger(constructor) {
     Object.seal(User);
-    return /** @class */ (function (_super) {
-        __extends(class_1, _super);
-        function class_1() {
-            var _this = _super !== null && _super.apply(this, arguments) || this;
-            _this.loginTime = (new Date().getHours() + "h:" + new Date().getMinutes() + "min");
-            return _this;
+    return class extends constructor {
+        constructor() {
+            super(...arguments);
+            this.loginTime = new Date().getHours() + "h:" + new Date().getMinutes() + "min";
         }
-        return class_1;
-    }(constructor));
+    };
+}
+function checkQuote() {
+    return (constructor, propKey) => {
+        var propertyVal = constructor[propKey];
+        const getter = () => propertyVal;
+        const setter = (value) => {
+            switch (value) {
+                case "Penso logo existo.":
+                    propertyVal = "Isso foi pensado por René Descartes.";
+                    break;
+                case "Só sei que nada sei.":
+                    propertyVal = "Isso foi pensado por Sócrates.";
+                    break;
+                default:
+                    propertyVal = "Realmente só sei que nada sei...";
+            }
+        };
+        Object.defineProperty(constructor, propKey, { get: getter, set: setter });
+    };
 }
 //Class Decorator
-var User = function () {
-    var _classDecorators = [logger];
-    var _classDescriptor;
-    var _classExtraInitializers = [];
-    var _classThis;
-    var User = _classThis = /** @class */ (function () {
-        function User_1(username, password) {
-            this.username = username;
-            this.password = password;
-            this.data = function () { return console.log({
-                username: username,
-                password: password,
-            }); };
-        }
-        return User_1;
-    }());
-    __setFunctionName(_classThis, "User");
-    (function () {
-        var _metadata = typeof Symbol === "function" && Symbol.metadata ? Object.create(null) : void 0;
-        __esDecorate(null, _classDescriptor = { value: _classThis }, _classDecorators, { kind: "class", name: _classThis.name, metadata: _metadata }, null, _classExtraInitializers);
-        User = _classThis = _classDescriptor.value;
-        if (_metadata) Object.defineProperty(_classThis, Symbol.metadata, { enumerable: true, configurable: true, writable: true, value: _metadata });
-        __runInitializers(_classThis, _classExtraInitializers);
-    })();
-    return User = _classThis;
-}();
-var newUser = new User("Gustavo", "Gustavo2409");
-if ("loginTime" in newUser)
-    console.log(newUser.loginTime);
+let User = class User {
+    constructor(username, password, quote) {
+        this.quote = quote;
+        this.data = () => console.log({
+            username: username,
+            password: password,
+        });
+    }
+};
+__decorate([
+    checkQuote()
+], User.prototype, "quote", void 0);
+User = __decorate([
+    logger
+], User);
+const newUser = new User("Gustavo", "Gustavo2409", "Penso logo existo.");
+console.log(newUser.quote);
--- decorators/index.ts
+++ decorators/index.ts
@@ -1,27 +1,48 @@
 function logger(constructor: any): any {
   Object.seal(User);
   return class extends constructor {
-    loginTime = (new Date().getHours() + "h:" + new Date().getMinutes() + "min");
-  }
+    loginTime = new Date().getHours() + "h:" + new Date().getMinutes() + "min";
+  };
+}
+
+function checkQuote() {
+  return (constructor: any, propKey: string) => {
+    var propertyVal = constructor[propKey];
+
+    const getter = () => propertyVal;
+    const setter = (value: string) => {
+      switch (value) {
+        case "Penso logo existo.":
+          propertyVal = "Isso foi pensado por René Descartes.";
+          break;
+        case "Só sei que nada sei.":
+          propertyVal = "Isso foi pensado por Sócrates.";
+          break;
+        default:
+          propertyVal = "Realmente só sei que nada sei...";
+      }
+    };
+
+    Object.defineProperty(constructor, propKey, { get: getter, set: setter });
+  };
 }
 
 //Class Decorator
 @logger
 class User {
+  @checkQuote()
+  quote;
   data;
 
-  constructor(
-    public username: string,
-    public password: string,
-  ) {
-    this.data = () => console.log({
-      username: username,
-      password: password,
-    });
+  constructor(username: string, password: string, quote: string) {
+    this.quote = quote;
+    this.data = () =>
+      console.log({
+        username: username,
+        password: password,
+      });
   }
-
 }
 
-const newUser = new User("Gustavo", "Gustavo2409")
-if ("loginTime" in newUser)
-  console.log(newUser.loginTime);
+const newUser = new User("Gustavo", "Gustavo2409", "Penso logo existo.");
+console.log(newUser.quote);
