9ffeaacc26267c0b161f7793d8c5b7261dde8456
Joao <dev.joaogustavo.dev@gmail.com>
2023-12-29 21:02:05 -0300

    studying typescript | generics in array and arrow functions

--- generics/index.js
+++ generics/index.js
@@ -4,3 +4,21 @@ function show(val) {
 }
 console.log(show(321));
 console.log(show({ item: "Hot Dog", price: 24.99 }));
+//Generics in Arrays
+function shift(val) {
+    if (val.length < 2) {
+        throw new Error("Arrays length should be greater than 1.");
+    }
+    val.shift();
+    return val[0];
+}
+var myArr = [32, 786, 54, 928];
+console.log(shift(myArr));
+var pop = function (val) {
+    if (val.length < 2) {
+        throw new Error("Arrays length should be greater than 1.");
+    }
+    val.pop();
+    return val[val.length - 1];
+};
+console.log("New last element: " + pop([1, 2]));
--- generics/index.ts
+++ generics/index.ts
@@ -1,13 +1,35 @@
 //Generics
 function show<T>(val: T): T {
-    return val;
+  return val;
 }
 
-console.log(show(321))
+console.log(show(321));
 
 interface order {
-    item: string,
-    price: number,
+  item: string;
+  price: number;
 }
 
-console.log(show<order>({ item: "Hot Dog", price: 24.99}));
+console.log(show<order>({ item: "Hot Dog", price: 24.99 }));
+
+//Generics in Arrays
+function shift<T>(val: Array<T>): T {
+  if (val.length < 2) {
+    throw new Error("Arrays length should be greater than 1.")
+  }
+  val.shift();
+  return val[0];
+}
+
+const myArr = [32, 786, 54, 928];
+console.log(shift(myArr));
+
+const pop = <T>(val: T[]): T => {
+  if (val.length < 2) {
+    throw new Error("Arrays length should be greater than 1.")
+  }
+  val.pop();
+  return val[val.length - 1];
+}
+
+console.log("New last element: " + pop([1, 2]));
